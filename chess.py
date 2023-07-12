dbe60a78fcef2fdaea20b359fc7fe1bd5b2cac89
Nemoliv <oliviasharp77@gmail.com>
2023-07-12 10:14:18 +1000

    Second commit

--- chess.py
+++ chess.py
@@ -4,11 +4,19 @@ Created on Tue Jul 11 13:08:29 2023
 
 @author: olivi
 """
+from board import init_position
+from board import init_board
+from board import print_board
+import pieces
 
-def init_board():
-    pass
-
-def
-
+def main():
+    board = init_board()
+    init_position(board)
+    print_board(board)
+    player_turn ="w"
+    pieces.king(board, 4, 4)
+    print_board(board)
+    
+    
 if __name__=="__main__":
     main()
